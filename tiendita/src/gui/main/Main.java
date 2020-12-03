d4fb4772d6a3f3d62e2c2cd3f3696c3c60b240ee
Saul Ytucayasi <saul.ytucayasi@upeu.edu.pe>
2020-12-03 16:59:02 -0500

    recontra bien

--- tiendita/src/gui/main/Main.java
+++ tiendita/src/gui/main/Main.java
@@ -6,9 +6,5 @@ public class Main {
     public static void main(String[] args) {
         Tiendita tiendita = new Tiendita();
         tiendita.setVisible(true);
-        
-        
-        
-        System.out.println("Hola que hace");
     } 
 }

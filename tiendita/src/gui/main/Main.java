ccdfe39845c50770060879274b87f83231ab070f
Ever1ck <64207368+Ever1ck@users.noreply.github.com>
2020-12-03 16:46:06 -0500

    Wenas :3

--- tiendita/src/gui/main/Main.java
+++ tiendita/src/gui/main/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Tiendita tiendita = new Tiendita();
         tiendita.setVisible(true);
-        System.out.println("Hola");
-        System.out.println("jack");
+        
+        
+        
+        System.out.println("Hola que hace");
     } 
 }

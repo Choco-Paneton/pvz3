01b5276432c5bbf27c377af88f5387fe2c18f5a9
JackSPari <71611947+JackSPari@users.noreply.github.com>
2020-12-03 16:43:58 -0500

    hi

--- tiendita/src/gui/main/Main.java
+++ tiendita/src/gui/main/Main.java
@@ -7,5 +7,6 @@ public class Main {
         Tiendita tiendita = new Tiendita();
         tiendita.setVisible(true);
         System.out.println("Hola");
+        System.out.println("jack");
     } 
 }

8980266dddab42db242c22c08b421b60a9d41dab
Saul Ytucayasi <saul.ytucayasi@upeu.edu.pe>
2020-12-03 16:38:07 -0500

    hola

--- tiendita/src/gui/main/Main.java
+++ tiendita/src/gui/main/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Tiendita tiendita = new Tiendita();
         tiendita.setVisible(true);
+        System.out.println("Hola");
     } 
 }

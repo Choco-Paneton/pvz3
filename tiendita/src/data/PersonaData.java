4e465952f0fdb6a15a3d827e0316313f9284bf37
JackSPari <71611947+JackSPari@users.noreply.github.com>
2020-12-06 22:28:41 -0500

    Update PersonaData.java
    listo espero que no malogre nd

--- tiendita/src/data/PersonaData.java
+++ tiendita/src/data/PersonaData.java
@@ -1,6 +1,171 @@
 
 package data;
 
+import entities.Persona;
+import util.ErrorLogger;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
 public class PersonaData {
 
+    static Connection cn = Coon_sqlite.connectSQLite();
+    static PreparedStatement ps;
+    static ErrorLogger log = new ErrorLogger(ClienteData.class.getName());
+
+    public static int create(Persona d) {
+        int rsId = 0;
+        String[] returns = {"id"};
+        String sql = "INSERT INTO persona(nombres, apellido_pa, apellido_ma, dni) "
+                + "VALUES(?,?)";
+        int i = 0;
+        try {
+            ps = cn.prepareStatement(sql, returns);
+            ps.setString(++i, d.getNombre());
+            ps.setString(++i, d.getApellido_paterno());
+            ps.setString(++i, d.getApellido_materno());
+            ps.setString(++i, d.getDni());
+            rsId = ps.executeUpdate();// 0 no o 1 si commit
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    rsId = rs.getInt(1); // select tk, max(id)  from clientes
+                    //System.out.println("rs.getInt(rsId): " + rsId);
+                }
+                rs.close();
+            }
+        } catch (SQLException ex) {
+            //System.err.println("create:" + ex.toString());
+            ErrorLogger.log(Level.SEVERE, "create", ex);
+        }
+        return rsId;
+    }
+
+    public static int update(Persona d) {
+        System.out.println("actualizar d.getId(): " + d.getId());
+        int comit = 0;
+        String sql = "UPDATE persona SET "
+                + "nombres=?, "
+                + "apelldo_pa=? "
+                + "apellido_ma=?"
+                + "dni=?"
+                + "WHERE id=?";
+        int i = 0;
+        try {
+            ps = cn.prepareStatement(sql);
+            ps.setString(++i, d.getNombre());
+            ps.setString(++i, d.getApellido_paterno());
+            ps.setString(++i, d.getApellido_materno());
+            ps.setString(++i, d.getDni());
+            ps.setInt(++i, d.getId());
+            comit = ps.executeUpdate();
+        } catch (SQLException ex) {
+            ErrorLogger.log(Level.SEVERE, "update", ex);
+        }
+        return comit;
+    }
+
+    public static int delete(int id) throws Exception {
+        int comit = 0;
+        String sql = "DELETE FROM persona WHERE id = ?";
+        try {
+            ps = cn.prepareStatement(sql);
+            ps.setInt(1, id);
+            comit = ps.executeUpdate();
+        } catch (SQLException ex) {
+            ErrorLogger.log(Level.SEVERE, "delete", ex);
+            // System.err.println("NO del " + ex.toString());
+            throw new Exception("Detalle:" + ex.getMessage());
+        }
+        return comit;
+    }
+
+    public static List<Persona> list(String filter) {
+        String filtert = null;
+        if (filter == null) {
+            filtert = "";
+        } else {
+            filtert = filter;
+        }
+        System.out.println("list.filtert:" + filtert);
+
+        List<Persona> ls = new ArrayList();
+
+        String sql = "";
+        if (filtert.equals("")) {
+            sql = "SELECT * FROM persona ORDER BY id";
+        } else {
+            sql = "SELECT * FROM persona WHERE (id LIKE'" + filter + "%' OR "
+                    + "nombres LIKE'" + filter + "%' OR apellidos_pa'" + filter + "%' OR apellidos_ma'" + filter + "%' OR dni'"
+                    + "id LIKE'" + filter + "%') "
+                    + "ORDER BY nombres";
+        }
+        try {
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                Persona d = new Persona();
+                d.setId(rs.getInt("id"));
+                d.setNombre(rs.getString("Nombre"));
+                d.setApellido_paterno(rs.getString("Ap Pa"));
+                d.setApellido_materno(rs.getString("Ap Ma"));
+                d.setDni(rs.getString("Dni"));
+                ls.add(d);
+            }
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "list", ex);
+        }
+        return ls;
+    }
+
+    public static Persona getByPId(int id) {
+        Persona d = new Persona();
+
+        String sql = "SELECT * FROM persona WHERE id = ? ";
+        int i = 0;
+        try {
+            ps = cn.prepareStatement(sql);
+            ps.setInt(++i, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                d.setId(rs.getInt("id"));
+                d.setNombre(rs.getString("Nombre"));
+                d.setApellido_paterno(rs.getString("Ap Pa"));
+                d.setApellido_materno(rs.getString("Ap Ma"));
+                d.setDni(rs.getString("Dni"));
+            }
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "getByPId", ex);
+        }
+        return d;
+    }
+    /*
+    public static void iniciarTransaccion() {
+        try {
+            cn.setAutoCommit(false);
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "iniciarTransaccion", ex);
+        }
+    }
+
+    public static void finalizarTransaccion() {
+        try {
+            cn.commit();
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "finalizarTransaccion", ex);
+        }
+    }
+
+    public static void cancelarTransaccion() {
+        try {
+            cn.rollback();
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "cancelarTransaccion", ex);
+        }
+    }
+     */
 }

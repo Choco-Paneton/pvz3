c62e7578c00eb49172ff1cd3d0b4e6256e732087
JhoselynQV <jhoselyn.quispe@upeu.edu.pe>
2020-12-07 07:13:29 -0500

    compra

--- tiendita/src/data/CompraData.java
+++ tiendita/src/data/CompraData.java
@@ -27,7 +27,7 @@ public class CompraData {
     static Date dateNowBuy = new Date();
     static String currentTime = sdf.format(dateNowBuy);
     
-    public static int create(Compra c) {
+    public static int create(Compra o) {
         int rsId = 0;
         String[] returns = {"id"};
         String sql = "INSERT INTO venta(fecha_venta, cliente_id)"
@@ -37,7 +37,7 @@ public class CompraData {
         try {
             ps = cn.prepareStatement(sql, returns);
             ps.setString(++i, currentTime);
-            ps.setInt(++i, c.getProveedor_id());
+            ps.setInt(++i, o.getProveedor_id());
             rsId = ps.executeUpdate();
             try (ResultSet rs = ps.getGeneratedKeys()) {
                 if (rs.next()) {
@@ -57,5 +57,114 @@ public class CompraData {
         return rsId;
     }
     
+    public static int update(Compra o) {
+        int comit = 0;
+        String sql = "UPDATE compra SET "
+                + "fecha_compra=?, "
+                + "proveedor_id=? "
+                + "WHERE id=?";
+        int i = 0;
+        try {
+            ps = cn.prepareStatement(sql);
+            ps.setString(++i, sdf.format(o.getFecha_compra()));
+            ps.setInt(++i, o.getProveedor_id());
+            ps.setInt(++i, o.getId());
+            comit = ps.executeUpdate();
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "update", ex);
+        }
+        return comit;
+    }
+    
+    
+    public static int delete(int id)  throws Exception{
+        int comit = 0;
+        String sql = "DELETE FROM compra WHERE id = ?";
+        try {
+            ps = cn.prepareStatement(sql);
+            ps.setInt(1, id);
+            comit = ps.executeUpdate();
+            
+            /*try {
+                    d.setFecha(sdf.parse(rs.getString("fecha")));
+                } catch (Exception e) {
+                }*/
+            
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "delete", ex);
+            // System.err.println("NO del " + ex.toString());
+            throw new Exception("Proveedor_id:" + ex.getMessage());
+        }
+        return comit;
+        
+    }
+    
+    public static List<Compra> list(String filter) {
+        String filtert = null;
+        if (filter == null) {
+            filtert = "";
+        } else {
+            filtert = filter;
+        }
+        
+        List<Compra> listVenta = new ArrayList();
+        String sql = "";
+        if (filtert.equals("")) {
+            sql = "SELECT o.*, r.ruc, p.nombre, p.apellido_paterno, p.apellido_materno FROM COMPRA as o"
+                   + "INNER JOIN Proveedor as r On r.id = o.proveedor_id "
+                   + "INNER JOIN Persona as p On p.id = r.persona_id "
+                   + "ORDER BY p.nombre ";
+        } else {
+            sql = "SELECT o.*, r.ruc, p.nombre, p.apellido_paterno, p.apellido_materno FROM VENTA as "
+                   + "INNER JOIN Proveedor as r On r.id = o.proveedor_id "
+                   + "INNER JOIN Persona as p On p.id = r.persona_id "
+                   + "where (r.ruc LIKE'" + filter + "%' OR "
+                   + " p.nombre LIKE'" + filter + "%' OR "
+                   + " p.apellido_paterno LIKE'" + filter + "%' OR "
+                   + " p.apellido_materno LIKE'" + filter + "%') "
+                   + "ORDER BY p.nombre ";
+     
+        }
+        try {
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                Compra v = new Compra();
+                v.setId(rs.getInt("id"));
+                v.setFecha_compra(rs.getDate("fecha_venta"));
+                /*try {
+                    d.setFecha(sdf.parse(rs.getString("fecha")));
+                } catch (Exception e) {
+                }*/
+                listVenta.add(v);
+            }
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "list", ex);
+        }
+        return listVenta;
+    }
+    
+    public static Compra getById(int id) {
+        Compra v = new Compra();
+        int i = 0;
+        String sql = "SELECT * FROM venta WHERE id = ? ";
+        try {
+            ps = cn.prepareStatement(sql);
+            ps.setInt(++i, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                v.setId(rs.getInt("id"));
+                v.setFecha_compra(rs.getDate("fecha_compra"));
+                v.setProveedor_id(rs.getInt("proveedor_id"));
+                /*try {
+                    d.setFecha(sdf.parse(rs.getString("fecha")));
+                } catch (Exception e) {
+                }*/
+            }
+        } catch (SQLException ex) {
+            log.log(Level.SEVERE, "getByPId", ex);
+        }
+        return v;
+    }
 
 }
